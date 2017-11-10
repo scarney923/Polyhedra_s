377455882806829c3279f839293a92e49c5d18aa
SASHA CARNEY <sasha.carney@coloradocollege.edu>
2017-11-10 12:47:39 -0600

    GreatIcos test

--- Canvas.java
+++ Canvas.java
@@ -20,7 +20,7 @@ public class Canvas extends JPanel {
 		setBackground(Color.black);
     //octahedron = new Octahedron();
     //dodecahedron = new Dodecahedron();
-    polyhedron = new GStellatedDodecahedron();
+    polyhedron = new GreatIcosahedron();
   }
 
   public void paintComponent(Graphics g){
---
+++ GreatIcosahedron.java
@@ -0,0 +1,15 @@
+
+public class GreatIcosahedron extends Polyhedron {
+
+  public GreatIcosahedron() {
+    super.number_of_vertices = 92;
+    super.number_of_faces = 240;
+    super.number_of_face_vertices = 3;
+
+
+    PolyhedronFileReader reader = new PolyhedronFileReader("./polyhedra_data/GreatIcosahedron.txt", 92, 240, 3 );
+    super.faces = reader.generate_faces_from_file_data();
+
+
+  }
+}
