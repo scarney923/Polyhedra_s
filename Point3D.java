9e63b53c859aed84a673cfa0dd76cbfbe3063b99
SASHA CARNEY <sasha.carney@coloradocollege.edu>
2017-11-12 14:55:48 -0600

    Point3D to cart

--- Point3D.java
+++ Point3D.java
@@ -7,34 +7,24 @@ public class Point3D{
   double x;
   double y;
   double z;
-  double w;
 
   public Point3D(double x, double y, double z){
     this.x=x;
     this.y=y;
     this.z=z;
-    this.w=1;
   }
 
   public Point3D(double[] coords){
     this.x=coords[0];
     this.y=coords[1];
     this.z=coords[2];
-    this.w=1;
   }
 
-  public void to_cartesian(){
-    x=x/w;
-    y=y/w;
-    z=z/w;
-    w=1;
-  }
   public double[] to_vector(){
     double[] v = new double[4];
     v[0] = x;
     v[1] = y;
     v[2] = z;
-    v[3] = w;
     return v;
   }
 
@@ -44,12 +34,10 @@ public class Point3D{
       x = v[0];
       y = v[1];
       z = v[2];
-      w = v[3];
-      to_cartesian();
   }
 
   public String toString(){
-    return "x: " + this.x + " y: " + this.y + " z: " + this.z + " w: " + this.w;
+    return "x: " + this.x + " y: " + this.y + " z: " + this.z;
   }
 
 
